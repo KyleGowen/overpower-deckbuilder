0005477aa94e7c4b30316c4f1cc1251e2e350cb1
KyleGowen <kyle@measurabl.com>
2025-10-05 00:14:19 +0000

    Enable Flyway migrations - migrations have been fixed
    - Changed SKIP_MIGRATIONS from true to false in both GitHub Actions and Terraform deployments
    - Migrations are now fixed and can run properly during deployment
    - This will ensure database schema is properly updated with latest changes

--- infra/user_data.sh
+++ infra/user_data.sh
@@ -119,7 +119,7 @@ docker run -d \
   -e DATABASE_URL="$DB_URL" \
   -e NODE_TLS_REJECT_UNAUTHORIZED=0 \
   -e PORT=3000 \
-  -e SKIP_MIGRATIONS=true \
+  -e SKIP_MIGRATIONS=false \
   ${ecr_repository_url}:latest
 
 # Wait a moment for the container to start
